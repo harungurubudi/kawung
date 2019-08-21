3c2efbbe1a7041bbe81b6711c1de824b384c228e
harunsadja <harunsadja@gmail.com>
2019-08-21 12:08:35 +0700

    Initial commit

---
+++ components/organisms/SiteHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container siteheader">
+      <div class="siteheader__logo">
+        <img src="~/static/img/logo.png" />
+      </div>
+      <nav>
+        <ul>
+          <li class="active">
+            <nuxt-link to="/">
+              Home
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/products">
+              Products
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/services">
+              Services
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about-us">
+              About Us
+            </nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/contact">
+              Contact
+            </nuxt-link>
+          </li>
+        </ul>
+      </nav>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <site-header />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import SiteHeader from '~/components/organisms/SiteHeader'
+
+export default {
+  components: {
+    SiteHeader
+  },
+  head() {
+    return {
+      htmlAttrs: {
+        lang: 'en',
+      }
+    }
+  }
+}
+</script>
